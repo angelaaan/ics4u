8ce58fadd1647f702a2ba90ad691b4f9e90bb2ea
angelaaan <114760476+angelaaan@users.noreply.github.com>
2022-11-28 23:35:06 -0500

    Update testr.java

--- testr.java
+++ testr.java
@@ -3,7 +3,6 @@ package Carnival;
 public class testr {
     public static void main(String[] args) {
         FoodCorner f1 = new FoodTruck() ;
-        f1.makePriceMenu();
         f1.printMenu();
     }
 }
