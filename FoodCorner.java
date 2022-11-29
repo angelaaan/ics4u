1f63f2c4f518eeef9491248fd147b6cf8552e457
angelaaan <114760476+angelaaan@users.noreply.github.com>
2022-11-28 23:34:36 -0500

    Update FoodCorner.java

--- FoodCorner.java
+++ FoodCorner.java
@@ -3,16 +3,16 @@ package Carnival;
 import java.util.Random;
 
 public abstract class FoodCorner {
+
     String[] menu = new String[7];
     int [] priceMenu = new int[7];
 
-    public FoodCorner (String[] newMenu, int[] newPriceMenu) {
+    public FoodCorner (String[] newMenu) {
         menu = newMenu;
-        priceMenu = newPriceMenu;
     }
 
     public abstract void printMenu();
 
-    public abstract void makePriceMenu();
+    public abstract int[] makeMenu();
     
 }
