5b3c2b7dbeacc2edd244a79aa1b6486cfc236e33
angelaaan <angelanguyen123u@gmail.com>
2022-12-04 19:07:52 -0500

    final final copy cuz luca

--- SmallPrize.java
+++ SmallPrize.java
@@ -1,9 +1,9 @@
 public class SmallPrize extends Prize{
 
-    private String[][] colourArr = {{"Black", "\u001B[30m"}, {"Red","\u001B[31m"},{"Green", "\u001B[30m"}};
+    private String[][] colourArr = {{"Black", "\u001B[30m"}, {"Red","\u001B[31m"},{"Green", "\u001B[32m"}};
 
     public SmallPrize(int balance){
-        super(balance, 5, "Small");
+        super(balance, 20, "Small");
         super.setColour(colourArr);
     }
 }
